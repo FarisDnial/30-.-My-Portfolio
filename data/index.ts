39e019893351cd17197e02513653abb299ce608b
FarisDnial <faris.danial.fauzi@gmail.com>
2025-05-06 11:49:41 +0800

    Initial Commit

---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    // { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm passionate about learning new technologies.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center items-center",
+        img: "/grid.svg",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Aspiring Full-stack Developer",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently exploring and learning several JavaScript animation libraries",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "Food Delivery Application",
+        des: "A web application for food delivery, featuring user authentication, order management, and payment processing.",
+        img: "/p1.svg",
+        iconLists: ["/re.svg", "/express.svg", "/mongodb.svg", "/stripe.svg"],
+        link: "https://food-del-frontend-vc8l.onrender.com",
+    },
+    {
+        id: 2,
+        title: "Twitter Clone",
+        des: "A social media platform for connecting with friends and sharing updates, featuring user authentication, post creation, and various interaction functionalities.",
+        img: "/p2.svg",
+        iconLists: ["/re.svg", "/express.svg", "/tail.svg", "/mongodb.svg", "/cloudinary.svg"],
+        link: "https://twitter-clone-xa7q.onrender.com",
+    },
+    {
+        id: 3,
+        title: "Lensify - Booking Application",
+        des: "A web application for managing bookings, featuring user authentication and CRUD operations for booking reservations",
+        img: "/p3.svg",
+        iconLists: ["/re.svg", "express.svg", "/tail.svg", "redux.svg", "firebase.svg"],
+        link: "https://booking-app-psi-eight.vercel.app/",
+    },
+    {
+        id: 4,
+        title: "Awwwards Winning Website Clone",
+        des: "A clone of an award-winning website, visually captivating and inspired by Zentry.",
+        img: "/p4.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/gsap.svg"],
+        link: "https://awwwards-bice-eight.vercel.app/",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Frontend Engineer Intern",
+        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Mobile App Dev - JSM Tech",
+        desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Freelance App Dev Project",
+        desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp3.svg",
+    },
+    {
+        id: 4,
+        title: "Lead Frontend Developer",
+        desc: "Developed and maintained user-facing features using modern frontend technologies.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/FarisDnial"
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+    },
+];
