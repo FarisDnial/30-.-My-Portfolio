000defc80d2a304b738f31d69f16ca58c2b50b59
FarisDnial <faris.danial.fauzi@gmail.com>
2025-05-08 11:49:44 +0800

    fixed "number "type in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,8 +30,8 @@ const Footer = () => {
                 </p>
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) => (
-                        <a href={profile.link} key={profile.id} target="_blank" rel="noopener noreferrer" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                            <img src={profile.img} alt={profile.id} width={20} height={20} />
+                        <a href={profile.link} key={profile.id.toString()} target="_blank" rel="noopener noreferrer" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
+                            <img src={profile.img} alt={profile.id.toString()} width={20} height={20} />
                         </a>
                     ))}
                 </div>
